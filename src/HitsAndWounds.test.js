48213480d4ae52192414cf1e313c1ab727b38bac
tooxoot <tooxoot.git@gmail.com>
2019-12-11 17:55:30 +0100

    Fix log (#2)
    * fixed log width
    
    * renamed workflow
    
    * added random funktion as injectable
    
    * added roller constructor test
    
    * css fix

--- src/HitsAndWounds.test.js
+++ src/HitsAndWounds.test.js
@@ -1,6 +1,6 @@
-import { getResult } from './HitsAndWounds'
+import { getResult, Roller } from './HitsAndWounds'
 
-let samplesize = 12000
+let samplesize = 6 ** 5
 let args = {
   count: samplesize,
   bs: 4,
@@ -16,239 +16,327 @@ let args = {
   rerollWoundFails: false,
   explodeWounds: false
 }
-const tolerance = 0.001
 
-let c = 0
-Math.random = () => (c === 6 ? (c = 1) : ++c) / 6
+const mockRandom = () => {
+  let c = 0
+  return () => (c === 6 ? (c = 1) : ++c) / 6
+}
+
+test('Roller constructor', () => {
+  const o = {}
+  expect(new Roller(o).random).toBe(o)
+  expect(new Roller().random).toBe(Math.random)
+})
 
 describe('Hits', () => {
   test('simple bs = 4', () => {
-    let count = getResult(args).slice(-1)[0].length
+    let roller = new Roller(mockRandom())
+    let count = getResult(args, roller).slice(-1)[0].length
     let expected = samplesize / 2
 
-    expect(Math.abs(count / expected - 1)).toBeLessThan(tolerance)
+    expect(count).toBe(expected)
   })
 
   test('simple bs = 6', () => {
-    let count = getResult({
-      ...args,
-      bs: 6
-    }).slice(-1)[0].length
+    let roller = new Roller(mockRandom())
+    let count = getResult(
+      {
+        ...args,
+        bs: 6
+      },
+      roller
+    ).slice(-1)[0].length
     let expected = samplesize / 6
 
-    expect(Math.abs(count / expected - 1)).toBeLessThan(tolerance)
+    expect(count).toBe(expected)
   })
 
   test('mod = +2', () => {
-    let count = getResult({
-      ...args,
-      hitmod: 2
-    }).slice(-1)[0].length
+    let roller = new Roller(mockRandom())
+    let count = getResult(
+      {
+        ...args,
+        hitmod: 2
+      },
+      roller
+    ).slice(-1)[0].length
     let expected = samplesize * (5 / 6)
 
-    expect(Math.abs(count / expected - 1)).toBeLessThan(tolerance)
+    expect(count).toBe(expected)
   })
 
   test('mod = -2', () => {
-    let count = getResult({
-      ...args,
-      hitmod: -2
-    }).slice(-1)[0].length
+    let roller = new Roller(mockRandom())
+    let count = getResult(
+      {
+        ...args,
+        hitmod: -2
+      },
+      roller
+    ).slice(-1)[0].length
     let expected = samplesize * (1 / 6)
 
-    expect(Math.abs(count / expected - 1)).toBeLessThan(tolerance)
+    expect(count).toBe(expected)
   })
 
   test('reroll ones', () => {
-    let count = getResult({
-      ...args,
-      rerollHitsOfOne: true
-    }).slice(-1)[0].length
+    let roller = new Roller(mockRandom())
+    let count = getResult(
+      {
+        ...args,
+        rerollHitsOfOne: true
+      },
+      roller
+    ).slice(-1)[0].length
     let expected = samplesize * (1 / 2 + 1 / 6 / 2)
 
-    expect(Math.abs(count / expected - 1)).toBeLessThan(tolerance)
+    expect(count).toBe(expected)
   })
 
   test('reroll fails', () => {
-    let count = getResult({
-      ...args,
-      rerollHitFails: true
-    }).slice(-1)[0].length
+    let roller = new Roller(mockRandom())
+    let count = getResult(
+      {
+        ...args,
+        rerollHitFails: true
+      },
+      roller
+    ).slice(-1)[0].length
     let expected = samplesize * (1 / 2 + 1 / 2 / 2)
 
-    expect(Math.abs(count / expected - 1)).toBeLessThan(tolerance)
+    expect(count).toBe(expected)
   })
 
   test('explode', () => {
-    let count = getResult({
-      ...args,
-      explodeHits: true
-    }).slice(-1)[0].length
+    let roller = new Roller(mockRandom())
+    let count = getResult(
+      {
+        ...args,
+        explodeHits: true
+      },
+      roller
+    ).slice(-1)[0].length
     let expected = samplesize * (1 / 2 + 1 / 6 / 2)
 
-    expect(Math.abs(count / expected - 1)).toBeLessThan(tolerance)
+    expect(count).toBe(expected)
   })
 
   test('reroll ones and fails', () => {
-    let count = getResult({
-      ...args,
-      rerollHitsOfOne: true,
-      rerollHitFails: true
-    }).slice(-1)[0].length
+    let roller = new Roller(mockRandom())
+    let count = getResult(
+      {
+        ...args,
+        rerollHitsOfOne: true,
+        rerollHitFails: true
+      },
+      roller
+    ).slice(-1)[0].length
     let expected = samplesize * (1 / 2 + 1 / 6 / 2)
     expected = expected + (samplesize - expected) / 2
 
-    expect(Math.abs(count / expected - 1)).toBeLessThan(tolerance)
+    expect(count).toBe(expected)
   })
 
   test('reroll ones and explode', () => {
-    let count = getResult({
-      ...args,
-      rerollHitsOfOne: true,
-      explodeHits: true
-    }).slice(-1)[0].length
+    let roller = new Roller(mockRandom())
+    let count = getResult(
+      {
+        ...args,
+        rerollHitsOfOne: true,
+        explodeHits: true
+      },
+      roller
+    ).slice(-1)[0].length
     let expected = samplesize * (1 / 2 + 1 / 6 / 2)
     let allSixes = samplesize * (1 / 6 + 1 / 6 / 6)
     expected = expected + allSixes * (1 / 2 + 1 / 6 / 2)
 
-    expect(Math.abs(count / expected - 1)).toBeLessThan(tolerance)
+    expect(count).toBe(expected)
   })
 })
 
 describe('hits and wounds', () => {
   test('simple t = s = 4', () => {
-    let count = getResult({
-      ...args,
-      doWounds: true
-    }).slice(-1)[0].length
+    let roller = new Roller(mockRandom())
+    let count = getResult(
+      {
+        ...args,
+        doWounds: true
+      },
+      roller
+    ).slice(-1)[0].length
     let expected = (samplesize / 2) * (3 / 6)
 
-    expect(Math.abs(count / expected - 1)).toBeLessThan(tolerance)
+    expect(count).toBe(expected)
   })
 
   test('simple t = 3, s = 4', () => {
-    let count = getResult({
-      ...args,
-      t: 3,
-      doWounds: true
-    }).slice(-1)[0].length
+    let roller = new Roller(mockRandom())
+    let count = getResult(
+      {
+        ...args,
+        t: 3,
+        doWounds: true
+      },
+      roller
+    ).slice(-1)[0].length
     let expected = (samplesize / 2) * (4 / 6)
 
-    expect(Math.abs(count / expected - 1)).toBeLessThan(tolerance)
+    expect(count).toBe(expected)
   })
 
   test('simple t = 1, s = 4', () => {
-    let count = getResult({
-      ...args,
-      t: 1,
-      doWounds: true
-    }).slice(-1)[0].length
+    let roller = new Roller(mockRandom())
+    let count = getResult(
+      {
+        ...args,
+        t: 1,
+        doWounds: true
+      },
+      roller
+    ).slice(-1)[0].length
     let expected = (samplesize / 2) * (5 / 6)
 
-    expect(Math.abs(count / expected - 1)).toBeLessThan(tolerance)
+    expect(count).toBe(expected)
   })
 
   test('simple t = 7, s = 4', () => {
-    let count = getResult({
-      ...args,
-      t: 7,
-      doWounds: true
-    }).slice(-1)[0].length
+    let roller = new Roller(mockRandom())
+    let count = getResult(
+      {
+        ...args,
+        t: 7,
+        doWounds: true
+      },
+      roller
+    ).slice(-1)[0].length
     let expected = (samplesize / 2) * (2 / 6)
 
-    expect(Math.abs(count / expected - 1)).toBeLessThan(tolerance)
+    expect(count).toBe(expected)
   })
 
   test('simple t = 16, s = 4', () => {
-    let count = getResult({
-      ...args,
-      t: 16,
-      doWounds: true
-    }).slice(-1)[0].length
+    let roller = new Roller(mockRandom())
+    let count = getResult(
+      {
+        ...args,
+        t: 16,
+        doWounds: true
+      },
+      roller
+    ).slice(-1)[0].length
     let expected = (samplesize / 2) * (1 / 6)
 
-    expect(Math.abs(count / expected - 1)).toBeLessThan(tolerance)
+    expect(count).toBe(expected)
   })
 
   test('mod = +2', () => {
-    let count = getResult({
-      ...args,
-      doWounds: true,
-      woundmod: 2
-    }).slice(-1)[0].length
+    let roller = new Roller(mockRandom())
+    let count = getResult(
+      {
+        ...args,
+        doWounds: true,
+        woundmod: 2
+      },
+      roller
+    ).slice(-1)[0].length
     let expected = (samplesize / 2) * (5 / 6)
 
-    expect(Math.abs(count / expected - 1)).toBeLessThan(tolerance)
+    expect(count).toBe(expected)
   })
 
   test('mod = -2', () => {
-    let count = getResult({
-      ...args,
-      doWounds: true,
-      woundmod: -2
-    }).slice(-1)[0].length
+    let roller = new Roller(mockRandom())
+    let count = getResult(
+      {
+        ...args,
+        doWounds: true,
+        woundmod: -2
+      },
+      roller
+    ).slice(-1)[0].length
     let expected = (samplesize / 2) * (1 / 6)
 
-    expect(Math.abs(count / expected - 1)).toBeLessThan(tolerance)
+    expect(count).toBe(expected)
   })
 
   test('reroll ones', () => {
-    let count = getResult({
-      ...args,
-      doWounds: true,
-      rerollWoundsOfOne: true
-    }).slice(-1)[0].length
+    let roller = new Roller(mockRandom())
+    let count = getResult(
+      {
+        ...args,
+        doWounds: true,
+        rerollWoundsOfOne: true
+      },
+      roller
+    ).slice(-1)[0].length
     let expected = (samplesize / 2) * (1 / 2 + 1 / 6 / 2)
 
-    expect(Math.abs(count / expected - 1)).toBeLessThan(tolerance)
+    expect(count).toBe(expected)
   })
 
   test('reroll fails', () => {
-    let count = getResult({
-      ...args,
-      doWounds: true,
-      rerollWoundFails: true
-    }).slice(-1)[0].length
+    let roller = new Roller(mockRandom())
+    let count = getResult(
+      {
+        ...args,
+        doWounds: true,
+        rerollWoundFails: true
+      },
+      roller
+    ).slice(-1)[0].length
     let expected = (samplesize / 2) * (1 / 2 + 1 / 2 / 2)
 
-    expect(Math.abs(count / expected - 1)).toBeLessThan(tolerance)
+    expect(count).toBe(expected)
   })
 
   test('explode', () => {
-    let count = getResult({
-      ...args,
-      doWounds: true,
-      explodeWounds: true
-    }).slice(-1)[0].length
+    let roller = new Roller(mockRandom())
+    let count = getResult(
+      {
+        ...args,
+        doWounds: true,
+        explodeWounds: true
+      },
+      roller
+    ).slice(-1)[0].length
     let expected = (samplesize / 2) * (1 / 2 + 1 / 6 / 2)
 
-    expect(Math.abs(count / expected - 1)).toBeLessThan(tolerance)
+    expect(count).toBe(expected)
   })
 
   test('reroll ones and fails', () => {
-    let count = getResult({
-      ...args,
-      doWounds: true,
-      rerollWoundsOfOne: true,
-      rerollWoundFails: true
-    }).slice(-1)[0].length
+    let roller = new Roller(mockRandom())
+    let count = getResult(
+      {
+        ...args,
+        doWounds: true,
+        rerollWoundsOfOne: true,
+        rerollWoundFails: true
+      },
+      roller
+    ).slice(-1)[0].length
     let expected = (samplesize / 2) * (1 / 2 + 1 / 6 / 2)
     expected = expected + (samplesize / 2 - expected) / 2
 
-    expect(Math.abs(count / expected - 1)).toBeLessThan(tolerance)
+    expect(count).toBe(expected)
   })
 
   test('reroll ones and explode', () => {
-    let count = getResult({
-      ...args,
-      doWounds: true,
-      rerollWoundsOfOne: true,
-      explodeWounds: true
-    }).slice(-1)[0].length
+    let roller = new Roller(mockRandom())
+    let count = getResult(
+      {
+        ...args,
+        doWounds: true,
+        rerollWoundsOfOne: true,
+        explodeWounds: true
+      },
+      roller
+    ).slice(-1)[0].length
     let expected = (samplesize / 2) * (1 / 2 + 1 / 6 / 2)
     let allSixes = (samplesize / 2) * (1 / 6 + 1 / 6 / 6)
     expected = expected + allSixes * (1 / 2 + 1 / 6 / 2)
 
-    expect(Math.abs(count / expected - 1)).toBeLessThan(tolerance)
+    expect(count).toBe(expected)
   })
 })
